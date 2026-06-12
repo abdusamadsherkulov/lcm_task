831b0f667d38f3dfe7428efcbb0e58ddae1c4ecf
Abdusamad Sherkulov <abdusamadsherkulov@MacBookPro.bbrouter>
2026-06-12 12:45:39 +0500

    lcm

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+function lcm(x, y) {
+    return (x * y) / gcd(x, y);
+}
+
+app.get('/abdusamad_sherkulov_gmail_com', (req, res) => {
+    const x = Number(req.query.x);
+    const y = Number(req.query.y);
+
+    if (
+        !Number.isInteger(x) ||
+        !Number.isInteger(y) ||
+        x < 1 ||
+        y < 1
+    ) {
+        return res.send('NaN');
+    }
+
+    return res.send(String(lcm(x, y)));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
