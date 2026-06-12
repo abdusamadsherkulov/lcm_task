cdd38a6c09649b2e84e05dd0f1b2d0fa4e3296ec
Abdusamad Sherkulov <abdusamadsherkulov@MacBookPro.bbrouter>
2026-06-12 12:50:09 +0500

    upd

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.get('/abdusamad_sherkulov_gmail_com', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running`);
 });
